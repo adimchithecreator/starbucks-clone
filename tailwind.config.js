c6ade55fc28693714cb63458af4982d4e741bd56
Adimchi <oraegbunamadimchi@gmail.com>
2022-07-25 19:19:29 +0100

    built a starbucks clone with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'peach':'#FA91AA',
+        'starGreen':'#007042',
+        'textGreen':'#1E3932',
+        'protein': '#9BF5B4',
+        'pride': '#D3E9E2',
+        'footerText':'rgba(0,0,0,.58)',
+        'navGreen':'#00754A',
+      }
+    },
+  },
+  plugins: [],
+}
